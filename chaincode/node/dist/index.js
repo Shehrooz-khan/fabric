bfa8055a96b914a3a3d73a85bfa290f6bd689a2d
Shehrooz khan <shehroozofficial@gmail.com>
2019-03-20 20:46:39 +0500

    First commit

---
+++ chaincode/node/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var shim = require("fabric-shim");
+var chaincode_1 = require("./chaincode");
+shim.start(new chaincode_1.default());
